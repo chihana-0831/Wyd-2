509be4e5b198664d27fddfa706f6397f9ed444c5
chihana-0831 <chihana_kashiwabara@rudel.jp>
2021-08-04 13:12:41 +0900

    created a page

---
+++ _body/servicepage.php
@@ -0,0 +1,13 @@
+<?php
+
+require('../_parts/functions.php');
+include('../_parts/header.php');
+
+?>
+
+<img src="../images/html.png" alt="logo" class="logo"> 
+
+<h1>Don't text,<br> Look Wyd!</h1>
+
+<?php
+include('../_parts/footer.php');
